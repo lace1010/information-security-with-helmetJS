259ccc37ad00ffb744d4cdc6692714713c747ede
Hunter Lacefield <lace1010@gmail.com>
2021-03-29 10:50:06 +0800

    added specific version of helmet for fcc

--- myApp.js
+++ myApp.js
@@ -1,60 +1,18 @@
-var express = require('express');
-var app = express();
+const express = require("express");
+const helmet = require("helmet");
+const app = express();
 
+app.use(helmet());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// don't edit below this note
 module.exports = app;
-var api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
+var api = require("./server.js");
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+app.use("/_api", api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
