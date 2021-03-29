1d9d120130b04d97c8b894c85473da03156a4ed8
Hunter Lacefield <lace1010@gmail.com>
2021-03-29 11:35:35 +0800

    add noSniff()

--- myApp.js
+++ myApp.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { xssFilter } = require("helmet");
+const { xssFilter, noSniff } = require("helmet");
 const helmet = require("helmet");
 const app = express();
 
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: "deny" }));
 app.use(xssFilter());
+app.use(noSniff());
 
 // don't edit below this note
 module.exports = app;
